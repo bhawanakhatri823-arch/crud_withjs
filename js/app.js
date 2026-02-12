8c07639ee21454e032482b01605671b6960cf3b7
bhawanakhatri823-arch <bhawanakhatri823@gmail.com>
2026-02-12 20:40:07 +0545

    first js coding

---
+++ js/app.js
@@ -0,0 +1,29 @@
+import { groceryItems } from "./data.js";
+import { createItems } from "./item.js";
+
+let items = groceryItems;
+
+// Render App
+function render() {
+  const app = document.getElementById("app");
+  app.innerHTML = "";
+
+  const itemsElement = createItems(items);
+  app.appendChild(itemsElement);
+}
+
+// Initialize App
+render();
+
+// ....
+
+// Edit Completed Function
+export function editCompleted(itemId) {
+  items = items.map((item) => {
+    if (item.id === itemId) {
+      return { ...item, completed: !item.completed };
+    }
+    return item;
+  });
+  render();
+}
